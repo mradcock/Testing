8d1cfc5584253aa09a7672665c2d9069cac62eb7
Martin Adcock <Marty@Kates-MacBook-Air.local>
2017-03-06 22:32:56 +0000

    hello world

--- Testing Repo/ViewController.swift
+++ Testing Repo/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    print("hello World")
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
